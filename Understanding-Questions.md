ee602b3f4eb31884c6c2e56851424bf1a99718b1
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-06-03 14:32:23 -0700

    Writes first 4 tests

--- Understanding-Questions.md
+++ Understanding-Questions.md
@@ -1,4 +1,12 @@
 # Understanding Questions:
+
 1. What are some possible tests for this application?
-* The component correctly renders.
-* ...
+
+- The component correctly renders.
+- ...
+  All inputs can receive text
+  Submit button can successfully submit data
+  Submitted data is successfully displayed
+  Error handling successfully works
+  Form won't submit if errors are present
+  Errors successfully display on screen
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,41 +1,47 @@
-import React from 'react';
-import {render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import React from "react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-import ContactForm from './ContactForm';
+import ContactForm from "./ContactForm";
 
-test('renders without errors', ()=>{
-    
+test("renders without errors", () => {
+  render(<ContactForm />);
 });
 
-test('renders the contact form header', ()=> {
-    
+test("renders the contact form header", () => {
+  render(<ContactForm />);
+  const header = screen.getByText(/contact form/i);
+  expect(header).toBeInTheDocument();
+  expect(header).toBeTruthy();
+  expect(header).toHaveTextContent(/contact form/i);
 });
 
-test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-    
+test("renders ONE error message if user enters less then 5 characters into firstname.", async () => {
+  render(<ContactForm />);
+  const firstNameInput = screen.getByLabelText(/first name/i);
+  userEvent.type(firstNameInput, "1234");
+  await waitFor(() => {
+    const error = screen.getByTestId("error");
+    expect(error).toBeInTheDocument();
+  });
 });
 
-test('renders THREE error messages if user enters no values into any fields.', async () => {
-    
+test("renders THREE error messages if user enters no values into any fields.", async () => {
+  render(<ContactForm />);
+  const submitButton = screen.getByRole("button");
+  userEvent.click(submitButton);
+  await waitFor(() => {
+    const error = screen.getAllByTestId("error");
+    expect(error.length).toBe(3);
+  });
 });
 
-test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
-    
-});
+test("renders ONE error message if user enters a valid first name and last name but no email.", async () => {});
 
-test('renders "email must be a valid email address" if an invalid email is entered', async () => {
-    
-});
+test('renders "email must be a valid email address" if an invalid email is entered', async () => {});
 
-test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
-    
-});
+test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {});
 
-test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
-    
-});
+test("renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.", async () => {});
 
-test('renders all fields text when all fields are submitted.', async () => {
-    
-});
+test("renders all fields text when all fields are submitted.", async () => {});
